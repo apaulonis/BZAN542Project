d19e1ed7827d1b03024ddea98ab611e74e11aca3
James McHale <mchalejd42@gmail.com>
2021-03-12 11:43:05 -0500

    Consolidating raw data into a json

---
+++ data_cleaning.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_json('dirty_data.json')
+
---
+++ raw_data/import_documents.py
@@ -0,0 +1,73 @@
+import os
+from sys import path
+from bs4 import BeautifulSoup, UnicodeDammit #pip install beautifulsoup4
+import lxml  #pip install lxml, used within beautiful soup call 'features'
+import pandas as pd
+import numpy as np
+
+class Reuters():
+    def __init__(self):
+        self.data = pd.DataFrame( {'newid': [np.nan], 
+                            'date': [np.nan], 
+                            'topics': [np.nan], 
+                            'places': [np.nan], 
+                            'people': [np.nan], 
+                            'orgs': [np.nan], 
+                            'exchanges': [np.nan], 
+                            'companies': [np.nan], 
+                            'unknown': [np.nan], 
+                            'title': [np.nan]} )
+        self.soup = None
+        self.paths = None
+
+    def make_paths(self):
+        self.paths = []
+        for ele in os.listdir('raw_data'):
+            if ele.endswith('.sgm'):
+                self.paths.append('raw_data/' + ele)
+
+    def make_soup(self, path):
+        fheader = open(path, 'r')
+        filetext = fheader.read()
+        fheader.close()
+        self.soup = BeautifulSoup(UnicodeDammit(filetext).unicode_markup, features= "lxml" )
+        
+
+    def get_documents(self):
+
+        df_import = self.data
+        
+        def get_children(text, document_index, data_dictionary = {}):
+            tag_text = data_dictionary
+            current_tag = None
+            current_text = None
+            for child in text.find_all('reuters')[document_index].findChildren():
+                if child.name in ['d', 'mknote']:
+                    continue
+                current_tag = child.name
+                current_text = child.text
+
+                if current_tag not in tag_text:
+                    tag_text[current_tag] = []
+                
+                tag_text[current_tag].append(current_text)
+            return tag_text
+            
+        for i, doc in enumerate(self.soup.find_all('reuters')):
+            tag_text = {'newid': str(doc.get("newid") ) }
+            tag_text = get_children(self.soup, i, data_dictionary= tag_text)    
+            df_import = df_import.append(tag_text, ignore_index= True )
+
+        self.data = df_import
+
+
+for filepath in docs.paths:
+    print(filepath)
+    docs.make_soup(filepath)
+    docs.get_documents()
+    print(docs.data.tail())
+
+docs.data.drop(0, axis= 0,inplace=True)
+
+docs.data.to_json()
+
