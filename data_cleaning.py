a3705e108d36aae39ad15d8de57a0f470db3c2e5
James McHale <mchalejd42@gmail.com>
2021-03-18 12:53:28 -0400

    unlisted, cleaned white space characters

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,3 +2,16 @@ import pandas as pd
 
 df = pd.read_json('dirty_data.json')
 
+singleelements = ['date', 'topics', 'places', 'people', 'orgs', 'exchanges', 'companies', 'title', 'dateline', 'text', 'author']
+
+for col in singleelements:
+    df[col] = df[col].explode()
+
+df.loc[df['unknown'].isna(), 'unknown'] = ['']
+df.loc[df['unknown'].notna(), ['unknown1','unknown2']] = pd.DataFrame(df.loc[df['unknown'].notna(),'unknown'].tolist(), index= df.index)
+
+df['text']=df['text'].replace(r'\n', '', regex=True)
+
+df.drop('unknown', axis= 1, inplace= True)
+
+#regex/ GREP through topics, people, places, orgs, exchanges
