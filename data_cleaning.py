100582d2290585c384e748f4ff99ef4e5b2979d9
Andrew Paulonis <apauloni@vols.utk.edu>
2021-03-19 10:30:16 -0400

    build a way to generate lookup tables from the raw files

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,24 @@
 import pandas as pd
 
+def generateLookup(path):
+    output = []
+    file = open(path, 'r')
+    lines = file.readlines()
+    
+    for line in lines:
+        line = line.replace('\n', '')
+        output.append(line)
+    
+    file.close()
+    return output
+
+topics_lookup = generateLookup('raw_data/all-topics-strings.lc.txt')
+places_lookup = generateLookup('raw_data/all-places-strings.lc.txt')
+people_lookup = generateLookup('raw_data/all-people-strings.lc.txt')
+orgs_lookup = generateLookup('raw_data/all-orgs-strings.lc.txt')
+exchanges_lookup = generateLookup('raw_data/all-exchanges-strings.lc.txt')
+    
+
 df = pd.read_json('dirty_data.json')
 
 singleelements = ['date', 'topics', 'places', 'people', 'orgs', 'exchanges', 'companies', 'title', 'dateline', 'text', 'author']
