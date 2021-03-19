e922b51a1f3fa569a24a88903197bf9414313870
Andrew Paulonis <apauloni@vols.utk.edu>
2021-03-19 11:12:32 -0400

    I updated all the columns to be list of items in the single string

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 def generateLookup(path):
     output = []
@@ -17,6 +18,8 @@ places_lookup = generateLookup('raw_data/all-places-strings.lc.txt')
 people_lookup = generateLookup('raw_data/all-people-strings.lc.txt')
 orgs_lookup = generateLookup('raw_data/all-orgs-strings.lc.txt')
 exchanges_lookup = generateLookup('raw_data/all-exchanges-strings.lc.txt')
+
+lookup_tables = [topics_lookup, places_lookup, people_lookup, orgs_lookup, exchanges_lookup]
     
 
 df = pd.read_json('dirty_data.json')
@@ -34,3 +37,18 @@ df['text']=df['text'].replace({r'\s+$': '', r'^\s+': ''}, regex=True).replace(r'
 df.drop('unknown', axis= 1, inplace= True)
 
 #regex/ GREP through topics, people, places, orgs, exchanges
+change_cols = df.columns[2:7]
+col_index = 0
+for col in change_cols:
+    current_lookup = lookup_tables[col_index]
+    row_index = 0
+    for row in df[col]:
+        new_row = []
+        if row != '':
+            for check_word in current_lookup:
+                find = re.findall(check_word, row)
+                if len(find) >0:
+                    new_row.append(find[0])
+        df.at[row_index, col] = new_row
+        row_index += 1
+    col_index += 1
