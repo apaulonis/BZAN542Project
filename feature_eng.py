f0b41ebf110437caf2b270a8f00dfde4cd35abad
James McHale <mchalejd42@gmail.com>
2021-03-27 11:49:07 -0400

    Combined money and currency, split corp

--- feature_eng.py
+++ feature_eng.py
@@ -1,5 +1,5 @@
 
-from re import T
+
 import pandas as pd
 #import plotly.express as px
 import random
@@ -13,6 +13,11 @@ This part generates a dictionary used to index the topics we have into the 6 bro
 "cat-descriptions_120396.txt"
 
 """
+
+"""
+First pass at categories, based on docs
+
+
 s_money_forex = {"MONEY-FX", "SHIP", "INTEREST"}
 s_economic_indicator = {"BOP", "TRADE", "CPI", "WPI", "JOBS", "IPI", "CPU", "GNP","MONEY-SUPPLY", "RESERVES", "LEI","HOUSING","INCOME","INVENTORIES","INSTAL-DEBT"," RETAIL"}
 s_currency = {"DLR","AUSTDLR","HK","SINGDLR","NZDLR","CAN","STG","DMK","YEN","SFR","FFR","BFR","DFL","LIT","DKR","NKR","SKR","MEXPESO","CRUZADO","AUSTRAL","SAUDRIYAL","RAND","RUPIAH","RINGGIT","ESCUDO","PESETA","DRACHMA"}
@@ -20,17 +25,43 @@ s_corporate = {"ACQ", "EARN"}
 s_commodity = {"ALUM","BARLEY","CARCASS","CASTOR-MEAL","CASTOR-OIL","CASTORSEED","CITRUSPULP","COCOA","COCONUT-OIL","COCONUT","COFFEE","COPPER","COPRA-CAKE","CORN-OIL","CORN","CORNGLUTENFEED","COTTON ","COTTON-MEAL","COTTON-OIL","COTTONSEED","F-CATTLE","FISHMEAL","FLAXSEED","GOLD","GRAIN","GROUNDNUT","GROUNDNUT-MEAL","GROUNDNUT-OIL","IRON-STEEL","LEAD","LIN-MEAL","LIN-OIL","LINSEED","LIVESTOCK","L-CATTLE","HOG","LUMBER","LUPIN","MEAL-FEED","NICKEL","OAT","OILSEED","ORANGE","PALLADIUM","PALM-MEAL","PALM-OIL","PALMKERNEL","PLATINUM","PLYWOOD","PORK-BELLY","POTATO","RAPE-MEAL","RAPE-OIL","RAPESEED","RED-BEAN","RICE","RUBBER","RYE","SILK","SILVER","SORGHUM","SOY-MEAL","SOY-OIL","SOYBEAN","STRATEGIC-METAL","SUGAR","SUN-MEAL","SUN-OIL","SUNSEED","TAPIOCA","TEA","TIN","TUNG-OIL","TUNG","VEG-OIL","WHEAT","WOOL","ZINC"}
 s_energy = {"CRUDE","HEAT","FUEL","GAS","NAT-GAS","PET-CHEM","PROPANE","JET","NAPHTHA"}
 
-s_money_forex = {x.lower() for x in money_forex}
-s_economic_indicator = {x.lower() for x in economic_indicator}
-s_currency = {x.lower() for x in currency}
-s_corporate = {x.lower() for x in corporate}
-s_commodity = {x.lower() for x in commodity}
-s_energy = {x.lower() for x in energy}
+s_money_forex = {x.lower() for x in s_money_forex}
+s_economic_indicator = {x.lower() for x in s_economic_indicator}
+s_currency = {x.lower() for x in s_currency}
+s_corporate = {x.lower() for x in s_corporate}
+s_commodity = {x.lower() for x in s_commodity}
+s_energy = {x.lower() for x in s_energy}
 
 subtopics = {'money': s_money_forex, 
             'econ': s_economic_indicator, 
             'currency': s_currency, 
             'corp': s_corporate,
+            'commodity': s_commodity,
+            'energy': s_energy}
+
+"""
+
+"""
+Combining categories/ Splitting Corporate
+"""
+s_money_forex = {"MONEY-FX", "SHIP", "INTEREST", "DLR","AUSTDLR","HK","SINGDLR","NZDLR","CAN","STG","DMK","YEN","SFR","FFR","BFR","DFL","LIT","DKR","NKR","SKR","MEXPESO","CRUZADO","AUSTRAL","SAUDRIYAL","RAND","RUPIAH","RINGGIT","ESCUDO","PESETA","DRACHMA"}
+s_economic_indicator = {"BOP", "TRADE", "CPI", "WPI", "JOBS", "IPI", "CPU", "GNP","MONEY-SUPPLY", "RESERVES", "LEI","HOUSING","INCOME","INVENTORIES","INSTAL-DEBT"," RETAIL"}
+s_acquisitions = {"ACQ"}
+s_earnings = {"EARN"}
+s_commodity = {"ALUM","BARLEY","CARCASS","CASTOR-MEAL","CASTOR-OIL","CASTORSEED","CITRUSPULP","COCOA","COCONUT-OIL","COCONUT","COFFEE","COPPER","COPRA-CAKE","CORN-OIL","CORN","CORNGLUTENFEED","COTTON ","COTTON-MEAL","COTTON-OIL","COTTONSEED","F-CATTLE","FISHMEAL","FLAXSEED","GOLD","GRAIN","GROUNDNUT","GROUNDNUT-MEAL","GROUNDNUT-OIL","IRON-STEEL","LEAD","LIN-MEAL","LIN-OIL","LINSEED","LIVESTOCK","L-CATTLE","HOG","LUMBER","LUPIN","MEAL-FEED","NICKEL","OAT","OILSEED","ORANGE","PALLADIUM","PALM-MEAL","PALM-OIL","PALMKERNEL","PLATINUM","PLYWOOD","PORK-BELLY","POTATO","RAPE-MEAL","RAPE-OIL","RAPESEED","RED-BEAN","RICE","RUBBER","RYE","SILK","SILVER","SORGHUM","SOY-MEAL","SOY-OIL","SOYBEAN","STRATEGIC-METAL","SUGAR","SUN-MEAL","SUN-OIL","SUNSEED","TAPIOCA","TEA","TIN","TUNG-OIL","TUNG","VEG-OIL","WHEAT","WOOL","ZINC"}
+s_energy = {"CRUDE","HEAT","FUEL","GAS","NAT-GAS","PET-CHEM","PROPANE","JET","NAPHTHA"}
+
+s_money_forex = {x.lower() for x in s_money_forex}
+s_economic_indicator = {x.lower() for x in s_economic_indicator}
+s_acquisitions = {x.lower() for x in s_acquisitions}
+s_earnings = {x.lower() for x in s_earnings}
+s_commodity = {x.lower() for x in s_commodity}
+s_energy = {x.lower() for x in s_energy}
+
+subtopics = {'money': s_money_forex, 
+            'econ': s_economic_indicator, 
+            'acquisitions': s_acquisitions,
+            'earnings' : s_earnings,
             'commodity': s_commodity,
             'energy': s_energy}
 
@@ -59,17 +90,7 @@ for el in df['topics']:
 len(all_topics)
 len(n_at_max)
 
-#Plots the distribution.
-#fig = px.histogram(n_at_max)
-#fig.show()
 
-#10,238- None; 10,856- 1 Label
-#432- 2 way ties; 29- 3 way ties; 1- 4 way ties; 22- 6 way ties
-
-
-"""
-Randomly select subtopic for ties in classification
-"""
 selected_topics = []
 for i in range(len(all_topics)):
     if n_at_max[i] == 0:
@@ -85,8 +106,8 @@ for i in range(len(all_topics)):
 
 selected_topics_series = pd.Series(selected_topics)
 
-#fig = px.histogram(x=selected_topics_series.dropna() )
-#fig.show()
+fig = px.histogram(x=selected_topics_series.dropna() )
+fig.show()
 
 #Create labels
 df['topic_label'] = selected_topics_series
@@ -98,3 +119,4 @@ df_labeled = df_labeled[['newid', 'date', 'topic_label', 'topics', 'places', 'pe
 
 #Adding Data
 df_labeled.to_json("labeled_data.json")
+
